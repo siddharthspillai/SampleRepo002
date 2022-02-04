da2dce67ae566c96ebbc9a31f4ff2673bbd3be24
Siddharth S Pillai <siddharth.pillai@yahoo.com>
2022-02-04 15:26:04 +0530

    Add Helper002.cs to main

---
+++ Source/SampleApplication_00033/SampleApplication_00033/Helper002.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication_00033
+{
+    class Helper002
+    {
+    }
+}
